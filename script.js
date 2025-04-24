14122c8bbbf4bdbc634be26d04e29eeba65649f0
HamidIqbal1928 <hamid313500@gmail.com>
2025-04-24 12:46:30 +0400

    add digital jpourney page

--- script.js
+++ script.js
@@ -5,3 +5,16 @@
   menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('show');
   });
+
+
+  // Gallery filter 
+  function filterGallery(category) {
+    const items = document.querySelectorAll('.gallery-item');
+    items.forEach(item => {
+      if (category === 'all' || item.classList.contains(category)) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
